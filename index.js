c630ef2ee55e33dd3032651b15ae9acd0442e3c0
Nishi <ngudai999@gmail.com>
2020-02-14 11:36:07 +0700

    Init a project to learn UI Design by CSS on Coders-x.com

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port, function() {
+    console.log(`Server UI Desige is running on port: ${port}`);
+});
